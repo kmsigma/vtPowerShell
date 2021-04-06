6fdd4f8c1143970570d3b769e5d7be5f6dbad5fd
ksparenberg <kevin.sparenberg@solarwinds.com>
2021-04-06 13:37:06 -0500

    Added AccountStatus, Presence, Join and Update

--- func_Users.ps1
+++ func_Users.ps1
@@ -1,25 +1,59 @@
 <#
 .Synopsis
-   Get a user id (or more information) from a Telligent Community
+    Get a user (or more information) from a Telligent Community
 .DESCRIPTION
-   TBD: Long description
+    Query the REST endpoint to get user account information.  Using the -All
 .EXAMPLE
-   TBD: Example of how to use this cmdlet
+    Get-VtUser -Username "JoeSmith" -CommunityDomain "https://mycommunity.teligenthosted.net" -AuthHeader @{ "Rest-User-Token" = "bG[REDACTED]]Q==" }
+    PS > Get-VtUser -UserId 112233 -CommunityDomain "https://mycommunity.teligenthosted.net" -AuthHeader @{ "Rest-User-Token" = "bG[REDACTED]]Q==" }
+    PS > Get-VtUser -EmailAddress "joseph.smith@company.com" -CommunityDomain "https://mycommunity.teligenthosted.net" -AuthHeader @{ "Rest-User-Token" = "bG[REDACTED]]Q==" }
+
+        Return a single user based on the their username, email address, or id
+    
+        --- All of the above returns the same output as below ---
+
+    UserId           : 112233
+    Username         : JoeSmith
+    EmailAddress     : joseph.smith@company.com
+    Status           : Approved
+    ModerationStatus : Unmoderated
+    CurrentPresence  : Offline
+    LastLogin        : 4/5/2021 9:44:21 PM
+    LastVisit        : 4/5/2021 9:44:37 PM
+    LifetimePoints   : 475
+
 .EXAMPLE
-   TBD: Another example of how to use this cmdlet
-.INPUTS
-   TBD: Inputs to this cmdlet (if any)
+    Get-VtUser -All -JoinDate ( ( Get-Date ).AddDays(-30) ) -Force
+
+    Get all users who have joined in the last 30 days.
+.EXAMPLE
+    $Global:CommunityDomain = "https://mycommunity.telligenthosted.net"
+    PS > $Global:AuthHeader       = Get-VtAuthHeader -Username "MyAdminAccount" -ApiKey "MyAdminApiKey"
+    PS > Get-VtUser -Username "JoeSmith"
+    
+    UserId           : 112233
+    Username         : JoeSmith
+    EmailAddress     : joseph.smith@company.com
+    Status           : Approved
+    ModerationStatus : Unmoderated
+    CurrentPresence  : Offline
+    LastLogin        : 4/5/2021 9:44:21 PM
+    LastVisit        : 4/5/2021 9:44:37 PM
+    LifetimePoints   : 475
+
+    Make calls against the API with Community Domain and Authentication Header stored as global variables
+
 .OUTPUTS
-   TBD: Output from this cmdlet (if any)
+    Without the ReturnDetails parameter, the function returns a custom PowerShell object containing the user Id,
+    the username, their email address, their account status, their moderation status, their last login, their last visit,
+    and their lifetime points.
+
+    With the ReturnDetails parameter, the function returns the entire JSON from the web call for each user.
 .NOTES
-   TBD: General notes
-   Source Documentation: https://community.telligent.com/community/11/w/api-documentation/64921/user-rest-endpoints
-.COMPONENT
-   TBD: The component this cmdlet belongs to
-.ROLE
-   TBD: The role this cmdlet belongs to
-.FUNCTIONALITY
-   TBD: The functionality that best describes this cmdlet
+    Tested with v11 of the Telligent Community platform using the User REST Endpoints
+    Source Documentation: https://community.telligent.com/community/11/w/api-documentation/64921/user-rest-endpoints
+
+    Relies on the Telligent and Utilities functions (defined in the 'begin' block )
 #>
 function Get-VtUser {
     [CmdletBinding(
@@ -86,6 +120,15 @@ function Get-VtUser {
         [ValidateNotNullOrEmpty()]
         [string]$FilterUsername,
 
+        # Are you absolutely sure that you want to run for all users?  Required for -All parameter.
+        [Parameter(
+            Mandatory=$false,
+            ParameterSetName='All'
+        )]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [switch]$Force = $false,
+
         # Batch size for API pulls for all users (1..100).  Defaults to API default of 20.
         [Parameter(
             Mandatory=$false,
@@ -98,6 +141,43 @@ function Get-VtUser {
 
         # Return all details instead of an abbreviated set
         [switch]$ReturnDetails = $false,
+
+        # Include hidden accounts (typically service accounts)
+        [Parameter(
+            Mandatory=$false,
+            ParameterSetName='All'
+        )]
+        [switch]$IncludeHidden = $false,
+
+        # Include only accounts joined after this date/time
+        [Parameter(
+            Mandatory=$false,
+            ParameterSetName='All'
+        )]
+        [datetime]$JoinDate,
+
+        # Include only accounts that have been updated after this date/time (automatically converts to UTC time)
+        [Parameter(
+            Mandatory=$false,
+            ParameterSetName='All'
+        )]
+        [datetime]$LastUpdated,
+
+        # Filter for account status - default is "All"
+        [Parameter(
+            Mandatory=$false,
+            ParameterSetName='All'
+        )]
+        [ValidateSet('Approved', 'ApprovalPending', 'Disapproved', 'Banned', 'All')]
+        [string]$AccountStatus = 'All',
+
+        # Filter for current presence status - default is neither 'Offline' nor 'Online'
+        [Parameter(
+            Mandatory=$false,
+            ParameterSetName='All'
+        )]
+        [ValidateSet('Online', 'Offline')]
+        [string]$Presence,
 
         # Community Domain to use (include trailing slash) Example: [https://yourdomain.telligenthosted.net/]
         [Parameter(
@@ -120,29 +200,60 @@ function Get-VtUser {
     begin {
         # Validate that the authentication header function  is available
         if ( -not ( Get-Command -Name Get-VtAuthHeader -ErrorAction SilentlyContinue ) ) {
-            . ".\func_Telligent.ps1"
+            . .\func_Telligent.ps1
         }
 
-        # Set default page index
+        if ( -not ( Get-Command -Name ConvertTo-QueryString -ErrorAction SilentlyContinue) ) {
+            . .\func_Utilities.ps1
+        }
+
+        # Set default page index, page size, and add any other filters
         $UriParameters = @{}
-        $UriParameters.Add("PageIndex", 0)
-        $UriParameters.Add("PageSize", $BatchSize)
+        # We only need to paginate if we are getting all users
+        if ( $All ) {
+            $UriParameters.Add("PageIndex", 0)
+            $UriParameters.Add("PageSize", $BatchSize)
+        }
+        if ( $IncludeHidden ) {
+            $UriParameters.Add("IncludeHidden", "true")
+        }
+        if ( $AccountStatus -ne 'All') {
+            $UriParameters.Add("AccountStatus", $AccountStatus)
+        }
+        if ( $JoinDate ) {
+            $UriParameters.Add("JoinDate", $JoinDate)
+        }
+        if ( $Presence ) {
+            $UriParameters.Add("Presence", $Presence)
+        }
+        if ( $LastUpdated ) {
+            $UriParameters.Add("LastUpdatedUtcDate", $LastUpdated.ToUniversalTime())
+        }
+    
+        if ( $All -and -not $Force ) {
+            # implies that someone wants all records, but didn't send over the Force parameter
+            Write-Warning -Message "Using the 'All' parameter requires the use of the 'Force' parameter.  This can take an incredible amount of time based on your environment."
+            break
+        }
     }
     process {
         switch ( $pscmdlet.ParameterSetName ) {
             'Username' { 
                 Write-Verbose -Message "Get-VtUser: Using the username [$Username] for the lookup"
-                $Uri = "api.ashx/v2/user.json?Username=$Username"
+                $UriParameters.Add("Username", $Username)
+                $Uri = "api.ashx/v2/user.json"
                 $LookupKey = $Username
             }
             'Email Address' {
                 Write-Verbose -Message "Get-VtUser: Using the email [$EmailAddress] for the lookup"
-                $Uri = "api.ashx/v2/user.json?EmailAddress=$EmailAddress"
+                $UriParameters.Add("EmailAddress", $EmailAddress)
+                $Uri = "api.ashx/v2/user.json"
                 $LookupKey = $EmailAddress
             }
             'User Id' {
                 Write-Verbose -Message "Get-VtUser: Using the user id [$UserId] for the lookup"
-                $Uri = "api.ashx/v2/user.json?Id=$UserId"
+                $UriParameters.Add("Id", $UserId)
+                $Uri = "api.ashx/v2/user.json"
                 $LookupKey = $UserId
             }
             'All' {
@@ -158,7 +269,6 @@ function Get-VtUser {
         }
         if ( $pscmdlet.ShouldProcess("$CommunityDomain", "Search for user with [$LookupKey]") ) {
             if ( $All ) {
-                Write-Warning -Message "Getting all users can take an incredible amount of time (even with a filter).  Use this functionality sparingly."
                 if ( $FilterEmail ) {
                     $FilterScript = { $_.PrivateEmail -like $FilterEmail }
                     $IsFiltered = $true
@@ -176,6 +286,7 @@ function Get-VtUser {
                 $StopWatch = New-Object -TypeName System.Diagnostics.Stopwatch
                 $StopWatch.Start()
                 do {
+                    Write-Verbose -Message "Uri: $( $CommunityDomain + $Uri + "?" + ( $UriParameters | ConvertTo-QueryString ) )"
                     $UserResponse = Invoke-RestMethod -Uri ( $CommunityDomain + $Uri + "?" + ( $UriParameters | ConvertTo-QueryString ) ) -Headers $AuthHeader
                     $UsersPerSecond = $UserResponse.Users.Count / $StopWatch.Elapsed.TotalSeconds
                     if ( $ReturnDetails ) {
@@ -203,25 +314,29 @@ function Get-VtUser {
                         } else {
                             $UserResponse.Users | Select-Object -Property UserId, Username, EmailAddress, Status, ModerationStatus, CurrentPresence, LastLogin, LastVisit, LifetimePoints
                         }
-                        #$UserResponse.Users | Select-Object -Property UserId, Username, EmailAddress, Status, CurrentPresence, LastLogin, LastVisit, Points
                     }
                     $TotalUsersReturned += $UserResponse.Users.Count
-                    Write-Progress -Activity "Getting All Users from $CommunityDomain" -CurrentOperation "Making Call #$( $UriParameters["PageIndex"] + 1) [$TotalUsersReturned / $( $UserResponse.TotalCount )]" -PercentComplete ( ( $TotalUsersReturned / $UserResponse.TotalCount ) * 100 ) -SecondsRemaining ( $UsersPerSecond * $UserResponse.TotalCount )
+                    if ( $TotalUsersReturned ) {
+                        Write-Progress -Activity "Getting All Users from $CommunityDomain" -CurrentOperation "Making Call #$( $UriParameters["PageIndex"] + 1) [$TotalUsersReturned results so far / $( $UserResponse.TotalCount ) total results]" -PercentComplete ( ( $TotalUsersReturned / $UserResponse.TotalCount ) * 100 ) -SecondsRemaining ( $UsersPerSecond * $UserResponse.TotalCount )
+                    }
 
                     # Increment Page Index
                     ( $UriParameters.PageIndex )++
 
-                } while ( ( $TotalUsersReturned -lt $UserResponse.TotalCount ) -or ( $UriParameters["PageIndex"] -eq 5 ) )
+                } while ( ( $TotalUsersReturned -lt $UserResponse.TotalCount ) )
                 Write-Progress -Activity "Getting All Users from $CommunityDomain" -Completed
-            } else {
+            } else { # Get a single user account
                 try {
-                    $UserResponse = Invoke-RestMethod -Uri ( $CommunityDomain + $Uri ) -Headers $AuthHeader
+                    Write-Verbose -Message "Executing in single user lookup mode"
+                    # Don't need page size or page index, so remove them from the UriParmeters
+                    Write-Verbose -Message "Uri: $( ( $CommunityDomain + $Uri + '?' + ( $UriParameters | ConvertTo-QueryString ) ) )"
+                    $UserResponse = Invoke-RestMethod -Uri ( $CommunityDomain + $Uri + '?' + ( $UriParameters | ConvertTo-QueryString ) ) -Headers $AuthHeader
                     if ( $UserResponse -and $ReturnDetails ) {
                         # We found a matching user, return everything with no pretty formatting
                         $UserResponse.User
                     } elseif ( $UserResponse ) { #implies '-not $ReturnDetails'
-                        # We found a matching user, build a custom PowerShell Object for it
                         # Return abbreviated data
+                        # We found a matching user, build a custom PowerShell Object for it
                         [PSCustomObject]@{
                             UserId = $UserResponse.User.id;
                             Username = $UserResponse.User.Username;
