4cd483b4041d162d758bc442ac4ea0ee39afbef3
ksparenberg <kevin.sparenberg@solarwinds.com>
2021-04-12 12:36:23 -0500

    Added Set-VtAuthHeader

--- func_Telligent.ps1
+++ func_Telligent.ps1
@@ -32,7 +32,12 @@
    The functionality that best describes this cmdlet
 #>
 function Get-VtAuthHeader {
-   [CmdletBinding()]
+   [CmdletBinding(
+      SupportsShouldProcess = $true, 
+      PositionalBinding = $false,
+      HelpUri = 'https://community.telligent.com/community/11/w/api-documentation/64803/list-point-transactions-point-transaction-rest-endpoint',
+      ConfirmImpact = 'Medium')
+   ]
    param (
       # Username for the call to the REST endpoint
       [Parameter(Mandatory = $true, 
@@ -51,24 +56,143 @@ function Get-VtAuthHeader {
          Position = 1)]
       [Alias("Key")] 
       [string]$ApiKey
-
-      # Yet to implement - or maybe we won't do it at all
-      # The "Rest-Method" header (Put, Post, Delete, etc...)
-
    )
-
 
    begin {
       # Nothing here
    }
 
    process {
-      $Base64Key = [Convert]::ToBase64String( [Text.Encoding]::UTF8.GetBytes( "$( $ApiKey ):$( $Username )" ) )
-      # Return the header with the token only
-      @{
-         'Rest-User-Token' = $Base64Key
+      if ( $pscmdlet.ShouldProcess("", "Generate Authentication Header") ) {
+         $Base64Key = [Convert]::ToBase64String( [Text.Encoding]::UTF8.GetBytes( "$( $ApiKey ):$( $Username )" ) )
+         # Return the header with the token only
+         @{
+            'Rest-User-Token' = $Base64Key
+         }
       }
+   }
+   
+   end {
+      #Nothing to see here        
+   }
+}
 
+<#
+.Synopsis
+   Update an existing authentication header for Verint | Telligent Community
+.DESCRIPTION
+   Add an optional REST Method for use with Update and Delete type calls
+.EXAMPLE
+   $AuthHeader | Set-VtAuthHeader -Method "Delete"
+
+   Name                           Value
+   ----                           -----
+   Rest-User-Token                bG1[omitted]dtYQ==
+   Rest-Method                    DELETE
+
+   Take an existing header and add "Delete" as the rest method
+
+.EXAMPLE
+   $AuthHeader
+
+   Name                           Value
+   ----                           -----
+   Rest-User-Token                bG1[omitted]dtYQ==
+   Rest-Method                    DELETE
+
+   PS > $AuthHeader | Set-VtAuthHeader -Method "Get"
+
+   Name                           Value
+   ----                           -----
+   Rest-User-Token                bG1[omitted]dtYQ==
+
+   "Get" style queries do not require a 'Rest-Mehod' in the header, so it is removed.  This is the same functionality as passing no RestMethod parameter.
+
+.EXAMPLE
+   $AuthHeader
+
+   Name                           Value
+   ----                           -----
+   Rest-User-Token                bG1[omitted]dtYQ==
+
+   PS > $DeleteHeader = $AuthHeader | Set-VtAuthHeader -Method "Delete"
+   PS > $DeleteHeader
+
+   Name                           Value
+   ----                           -----
+   Rest-User-Token                bG1[omitted]dtYQ==
+   Rest-Method                    DELETE
+
+   PS > $UpdateHeader = $AuthHeader | Set-VtAuthHeader -Method "Put"
+   PS > $UpdateHeader
+
+   Name                           Value
+   ----                           -----
+   Rest-User-Token                bG1[omitted]dtYQ==
+   Rest-Method                    PUT
+
+   Create two new headers ($DeleteHeader and $UpdateHeader) based on the original header ($AuthHeader)
+
+.INPUTS
+   Existing Authentication Header (as Hashtable)
+.OUTPUTS
+   Hashtable with necessary headers
+.NOTES
+   https://community.telligent.com/community/10/w/developer-training/53138/authentication#Using_an_API_Key_and_username_to_authenticate_REST_requests
+
+#>
+function Set-VtAuthHeader {
+   [CmdletBinding(
+      SupportsShouldProcess = $true, 
+      PositionalBinding = $false,
+      HelpUri = 'https://community.telligent.com/community/11/w/api-documentation/64803/list-point-transactions-point-transaction-rest-endpoint',
+      ConfirmImpact = 'Medium')
+   ]
+   param (
+      # Existing authentication header
+      [Parameter(Mandatory = $true, 
+         ValueFromPipeline = $true,
+         ValueFromPipelineByPropertyName = $true, 
+         ValueFromRemainingArguments = $false, 
+         Position = 0)]
+      [Alias("Header")] 
+      [System.Collections.Hashtable[]]$AuthHeader,
+
+      # Rest-Method to invoke
+      [Parameter(Mandatory = $false, 
+         Position = 1)]
+      [ValidateSet("Get", "Put", "Delete")] # There may be others
+      [Alias("Method")] 
+      [string]$RestMethod = "Get"
+   )
+
+   begin {
+      # Nothing here
+   }
+
+   process {
+      # Support multiple tokens (this should be rare)
+      ForEach ( $h in $AuthHeader ) {
+         
+         if ( $h["Rest-User-Token"] ) {
+            if ( $pscmdlet.ShouldProcess("Header with 'Rest-User-Token: $( $h["Rest-User-Token"] )'", "Update Rest-Method to $RestMethod type") ) {
+               if ( $RestMethod -ne "Get" ) {
+               
+                  # Add a Rest-Method to the Token
+                  $h["Rest-Method"] = $RestMethod.ToUpper()
+               }
+               else {
+                  # 'Get' does not require the method
+                  $h.Remove("Rest-Method")
+               }
+               # Return the new header
+               $h
+            }
+         }
+         else {
+            Write-Error -Message "Header does not contain the 'Rest-User-Token' - Please generate a valid token with Get-VtAuthHeader"
+         }
+      }
    }
 
    end {
