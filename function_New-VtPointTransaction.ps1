f226a2d687e9d40aff5e3e78b91d9f69d26099c8
kmsigma <kevin.sparenberg@solarwinds.com>
2022-09-28 17:37:46 -0400

    Some Points Work

--- function_New-VtPointTransaction.ps1
+++ function_New-VtPointTransaction.ps1
@@ -20,6 +20,7 @@ function New-VtPointTransaction {
         The functionality that best describes this cmdlet
     #>
     [CmdletBinding(
+        DefaultParameterSetName = 'Username with Connection File',
         SupportsShouldProcess = $true, 
         PositionalBinding = $false,
         HelpUri = 'https://community.telligent.com/community/11/w/api-documentation/64799/point-transaction-rest-endpoints',
@@ -31,11 +32,47 @@ function New-VtPointTransaction {
         [Parameter(
             Mandatory = $true, 
             ValueFromPipeline = $true,
-            ValueFromPipelineByPropertyName = $true
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'Username with Authentication Header'
+        )]
+        [Parameter(
+            Mandatory = $true, 
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'Username with Connection Profile'
+        )]
+        [Parameter(
+            Mandatory = $true, 
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'Username with Connection File'
         )]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
         [string[]]$Username,
+        
+        # The username of the account who is getting the points
+        [Parameter(
+            Mandatory = $true, 
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'User ID with Authentication Header'
+        )]
+        [Parameter(
+            Mandatory = $true, 
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'User ID with Connection Profile'
+        )]
+        [Parameter(
+            Mandatory = $true, 
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'User ID with Connection File'
+        )]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [int64[]]$UserId,
     
         # The number of points to award to an account.  This does not support removing points.
         [Parameter(
@@ -57,80 +94,138 @@ function New-VtPointTransaction {
         [ValidateLength(0, 250)]
         [string]$Description,
     
-    
         # The date/time you want to have the awards added - defaults to 'now'
         [Parameter(
             Mandatory = $false, 
             Position = 3
         )]
-        [datetime]$AwardDateTime = ( Get-VtDate ),
-    
-        # Community Domain to use (include trailing slash) Example: [https://yourdomain.telligenthosted.net/]
+        [datetime]$AwardDateTime = ( Get-Date ),
+
+        # Optional ContentId
         [Parameter(
-            Mandatory = $false
+            Mandatory = $false, 
+            Position = 4
         )]
+        [guid]$ContentId,
+
+        # Optional ContentTypeId
+        [Parameter(
+            Mandatory = $false,
+            Position = 5
+        )]
+        [guid]$ContentTypeId,
+
+        [Parameter()]
+        [switch]$ReturnDetails,
+    
+        [Parameter(Mandatory = $true, ParameterSetName = 'Username with Authentication Header')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'User ID with Authentication Header')]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
         [ValidatePattern('^(http:\/\/|https:\/\/)(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])\/$')]
-        [string]$VtCommunity = $Global:VtCommunity,
-    
+        [Alias("Community")]
+        [string]$VtCommunity,
+        
         # Authentication Header for the community
-        [Parameter(
-            Mandatory = $false
-        )]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Username with Authentication Header')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'User ID with Authentication Header')]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
-        [System.Collections.Hashtable]$VtAuthHeader = $Global:VtAuthHeader
+        [Alias("AuthHeader")]
+        [System.Collections.Hashtable]$VtAuthHeader,
+    
+        [Parameter(Mandatory = $true, ParameterSetName = 'Username with Connection Profile')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'User ID with Connection Profile')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSObject]$Connection,
+    
+        # File holding credentials.  By default is stores in your user profile \.vtPowerShell\DefaultCommunity.json
+        [Parameter(ParameterSetName = 'Username with Connection File')]
+        [Parameter(ParameterSetName = 'User ID with Connection File')]
+        [string]$ProfilePath = ( $env:USERPROFILE ? ( Join-Path -Path $env:USERPROFILE -ChildPath ".vtPowerShell\DefaultCommunity.json" ) : ( Join-Path -Path $env:HOME -ChildPath ".vtPowerShell/DefaultCommunity.json" ) )
     )
     
     BEGIN {
     
+        switch -wildcard ( $PSCmdlet.ParameterSetName ) {
+
+            '* Connection File' {
+                Write-Verbose -Message "Getting connection information from Connection File ($ProfilePath)"
+                $VtConnection = Get-Content -Path $ProfilePath | ConvertFrom-Json
+                $Community = $VtConnection.Community
+                # Check to see if the VtAuthHeader is empty
+                $AuthHeaders = @{ }
+                $VtConnection.Authentication.PSObject.Properties | ForEach-Object { $AuthHeaders[$_.Name] = $_.Value }
+            }
+            '* Connection Profile' {
+                Write-Verbose -Message "Getting connection information from Connection Profile"
+                $Community = $Connection.Community
+                $AuthHeaders = $Connection.Authentication
+            }
+            '* Authentication Header' {
+                Write-Verbose -Message "Getting connection information from Parameters"
+                $Community = $VtCommunity
+                $AuthHeaders = $VtAuthHeader
+            }
+        }
+
         $Uri = "api.ashx/v2/pointtransactions.json"
-    
-        $RestMethod = "GET"
+
+        $PropertiesToReturn = @(
+            @{ Name = "UserId"; Expression = { $_.User.id } }
+            @{ Name = "Username"; Expression = { $_.User.Username } }
+            'CreatedDate'
+            'Value'
+            @{ Name = 'Content'; Expression = { $_.Content.HtmlName | ConvertFrom-HtmlString } }
+            @{ Name = 'Transaction'; Expression = { $_.id } }
+        )
     }
+    
     PROCESS {
     
-        ForEach ( $U in $Username ) {
-            $Proceed = $true
-    
-            # Convert the username to a userid
-            $User = Get-VtUser -Username $U -AuthHeader $VtAuthHeader -Community $VtCommunity -ReturnDetails -WhatIf:$false
-            # Since point transactions require a ContentId and ContentTypeId, we should pull those from the user's profile
-            if ( -not $User ) {
-                Write-Error -Message "Unable to add point because we didn't find a matching user for [$U]"
-                $Proceed = $false
+        if ( $PSCmdlet.ParameterSetName -like 'Username *' ) {
+            # By Username
+            # Convert Usernames to User IDs
+
+            $UserId = $Username | ForEach-Object { Get-VtUser -Username $_ -Community $Community -AuthHeader $AuthHeaders -ErrorAction SilentlyContinue | Select-Object -ExpandProperty UserId }
+        }
+
+        ForEach ( $U in $UserId ) {
+            if ( -not $ContentId -or $ContentTypeId ) {
+                $User = Get-VtUser -UserId $U -Community $Community -AuthHeader $AuthHeaders -ReturnDetails -ErrorAction SilentlyContinue
             }
-            else {
-                $Userid = $User.Id
+
+            if ( -not $ContentId ) {
                 $ContentId = $User.ContentId
+            }
+
+            if ( -not $ContentTypeId ) {
                 $ContentTypeId = $User.ContentTypeId
             }
-            
-            if ($PSCmdlet.ShouldProcess("User: $( $User.DisplayName )", "Add $Points points") -and $Proceed) {
-                if ( $Points -gt 5000 ) {
-                    Write-Host "====LARGE POINT DISTRIBUTION VALIDATION====" -ForegroundColor Yellow
-                    $BigPoints = Read-Host -Prompt "You are about to add $Points to $( $U.DisplayName  )'s account.  Are you sure you want to do this?  [Enter 'Yes' to confirm]"
-                    $Proceed = ( $BigPoints.ToLower() -eq 'yes' )
+
+            if ( $PSCmdlet.ShouldProcess("User: $( $User.DisplayName )", "Add $Points points") ) {
+
+                $PointsBody = @{
+                    Description   = '<p>' + $Description + '</p>'
+                    UserId        = $U
+                    Value         = $Points
+                    ContentId     = $ContentId
+                    ContentTypeId = $ContentTypeId
+                    CreatedDate   = $AwardDateTime
                 }
-                if ( $Proceed ) {
-                    # Build the body to send to the account
-                    # For proper display, the description needs to be wrapped in HTML paragraph tags.
-                    $Body = @{
-                        Description   = "<p>$Description</p>";
-                        UserId        = $UserId;
-                        Value         = $Points;
-                        ContentID     = $ContentId; # We'll just use the ContentID from the user
-                        ContentTypeId = $ContentTypeId; # We'll just use the ContentID from the user
-                        CreatedDate   = $AwardDateTime;
-                    }
-                    try {
-                        $PointsRequest = Invoke-RestMethod -Uri ( $Community + $Uri ) -Method Post -Body $Body -Headers ( $VtAuthHeader | Set-VtAuthHeader -RestMethod $RestMethod -Verbose:$False -WhatIf:$false )
+
+                try {
+                    $PointsRequest = Invoke-RestMethod -Uri ( $Community + $Uri ) -Method 'Post' -Body $PointsBody -Headers $AuthHeaders
+                    if ( $ReturnDetails ) {
                         $PointsRequest.PointTransaction
                     }
-                    catch {
-                        Write-Error -Message "Something didn't work"
+                    else {
+                        $PointsRequest.PointTransaction | Select-Object -Property $PropertiesToReturn
                     }
+                }
+                catch {
+                    Write-Error -Message "Something didn't work when trying to assign $Points to User ID: $U"
                 }
             }
         }
--- function_Remove-VtPointTransaction.ps1
+++ function_Remove-VtPointTransaction.ps1
@@ -20,6 +20,7 @@ function Remove-VtPointTransaction {
         The functionality that best describes this cmdlet
     #>
     [CmdletBinding(
+        DefaultParameterSetName = 'Transaction ID with Connection File',    
         SupportsShouldProcess = $true, 
         PositionalBinding = $false,
         HelpUri = 'https://community.telligent.com/community/11/w/api-documentation/64801/delete-point-transaction-point-transaction-rest-endpoint',
@@ -32,45 +33,91 @@ function Remove-VtPointTransaction {
             Mandatory = $true, 
             ValueFromPipeline = $true,
             ValueFromPipelineByPropertyName = $true, 
-            ValueFromRemainingArguments = $false
+            ValueFromRemainingArguments = $false,
+            ParameterSetName = 'Transaction ID with Authentication Header'
+        )]
+        [Parameter(
+            Mandatory = $true, 
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true, 
+            ValueFromRemainingArguments = $false,
+            ParameterSetName = 'Transaction ID with Connection Profile'
+        )]
+        [Parameter(
+            Mandatory = $true, 
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true, 
+            ValueFromRemainingArguments = $false,
+            ParameterSetName = 'Transaction ID with Connection File'
         )]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
-        [int[]]$TransactionId,
+        [int64[]]$TransactionId,
+
+        [Parameter()]
+        [switch]$ReturnDetails,
     
-        # Community Domain to use (include trailing slash) Example: [https://yourdomain.telligenthosted.net/]
-        [Parameter(
-            Mandatory = $false
-        )]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Transaction ID with Authentication Header')]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
         [ValidatePattern('^(http:\/\/|https:\/\/)(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])\/$')]
-        [string]$VtCommunity = $Global:VtCommunity,
-    
+        [Alias("Community")]
+        [string]$VtCommunity,
+        
         # Authentication Header for the community
-        [Parameter(
-            Mandatory = $false
-        )]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Transaction ID with Authentication Header')]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
-        [System.Collections.Hashtable]$VtAuthHeader = $Global:VtAuthHeader
+        [Alias("AuthHeader")]
+        [System.Collections.Hashtable]$VtAuthHeader,
+    
+        [Parameter(Mandatory = $true, ParameterSetName = 'Transaction ID with Connection Profile')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSObject]$Connection,
+    
+        # File holding credentials.  By default is stores in your user profile \.vtPowerShell\DefaultCommunity.json
+        [Parameter(ParameterSetName = 'Transaction ID with Connection File')]
+        [string]$ProfilePath = ( $env:USERPROFILE ? ( Join-Path -Path $env:USERPROFILE -ChildPath ".vtPowerShell\DefaultCommunity.json" ) : ( Join-Path -Path $env:HOME -ChildPath ".vtPowerShell/DefaultCommunity.json" ) )
     )
     BEGIN {
     
+        switch -wildcard ( $PSCmdlet.ParameterSetName ) {
+
+            '* Connection File' {
+                Write-Verbose -Message "Getting connection information from Connection File ($ProfilePath)"
+                $VtConnection = Get-Content -Path $ProfilePath | ConvertFrom-Json
+                $Community = $VtConnection.Community
+                # Check to see if the VtAuthHeader is empty
+                $AuthHeaders = @{ }
+                $VtConnection.Authentication.PSObject.Properties | ForEach-Object { $AuthHeaders[$_.Name] = $_.Value }
+            }
+            '* Connection Profile' {
+                Write-Verbose -Message "Getting connection information from Connection Profile"
+                $Community = $Connection.Community
+                $AuthHeaders = $Connection.Authentication
+            }
+            '* Authentication Header' {
+                Write-Verbose -Message "Getting connection information from Parameters"
+                $Community = $VtCommunity
+                $AuthHeaders = $VtAuthHeader
+            }
+        }
+
         $RestMethod = "Delete"
     
     }
     PROCESS {
         # This is where things do stuff
         ForEach ( $id in $TransactionId ) {
-            if ( Get-VtPointTransaction -TransactionId $Id -Community $VtCommunity -AuthHeader ( $VtAuthHeader | Set-VtAuthHeader -RestMethod Get -WhatIf:$false -Verbose:$false ) -Verbose:$false ) {
-                if ( $PSCmdlet.ShouldProcess("$VtCommunity", "Delete Point Transaction $id") ) {
+            #if ( Get-VtPointTransaction -TransactionId $Id -Community $Community -AuthHeader ( $AuthHeaders | Update-VtAuthHeader -RestMethod Get -WhatIf:$false -Verbose:$false ) -Verbose:$false ) {
+                if ( $PSCmdlet.ShouldProcess("$Community", "Delete Point Transaction $id") ) {
                     
                     $Uri = "api.ashx/v2/pointtransaction/$( $id ).json"
                     # Method: Post
                     # Rest-Method: Delete
                     try {
-                        $RemovePointsResponse = Invoke-RestMethod -Method Post -Uri ( $Community + $Uri ) -Headers ( $VtAuthHeader | Set-VtAuthHeader -RestMethod $RestMethod -WhatIf:$false -Verbose:$false )
+                        $RemovePointsResponse = Invoke-RestMethod -Method Post -Uri ( $Community + $Uri ) -Headers ( $AuthHeaders | Update-VtAuthHeader -RestMethod $RestMethod -WhatIf:$false -Verbose:$false )
                         if ( $RemovePointsResponse ) {
                             Write-Verbose -Message "Points Transaction #$id removed"
                         }
@@ -80,10 +127,10 @@ function Remove-VtPointTransaction {
                     }
                 }
                     
-            }
-            else {
-                Write-Verbose -Message "No points transaction found matching #$id.  Unable to delete."
-            }
+            #}
+            #else {
+            #    Write-Verbose -Message "No points transaction found matching #$id.  Unable to delete."
+            #}
         }
     
     }
