83ee1a9d9d33406c8978c38ce77e66d271c6e673
kmsigma <kevin.sparenberg@solarwinds.com>
2023-03-09 14:01:24 -0500

    Fixed query for all - still to test all

--- function_Get-VtChallenge.ps1
+++ function_Get-VtChallenge.ps1
@@ -21,10 +21,10 @@ function Get-VtChallenge {
     .FUNCTIONALITY
         The functionality that best describes this cmdlet
     #>
-    [CmdletBinding(DefaultParameterSetName = 'All Groups', 
+    [CmdletBinding(DefaultParameterSetName = 'All ideations with Connection File', 
         SupportsShouldProcess = $true, 
         PositionalBinding = $false,
-        HelpUri = 'https://community.telligent.com/community/11/w/api-documentation/64699/group-rest-endpoints',
+        HelpUri = 'https://community.telligent.com/community/11/w/api-documentation/64555/challenge-rest-endpoints',
         ConfirmImpact = 'Medium')]
     [Alias()]
     [OutputType()]
@@ -32,35 +32,30 @@ function Get-VtChallenge {
     (
 
         # Get challenge by name
-        [Parameter(
-            ParameterSetName = 'By Name')]
+        [Parameter(ParameterSetName = 'Ideation by Name with Authentication Header')]
+        [Parameter(ParameterSetName = 'Ideation by Name with Connection Profile')]
+        [Parameter(ParameterSetName = 'Ideation by Name with Connection File')]
         [string]$Name,
         
         # Challenge name exact match
-        [Parameter(
-            ParameterSetName = 'By Name')]
+        [Parameter(ParameterSetName = 'Ideation by Name with Authentication Header')]
+        [Parameter(ParameterSetName = 'Ideation by Name with Connection Profile')]
+        [Parameter(ParameterSetName = 'Ideation by Name with Connection File')]
         [switch]$ExactMatch = $false,
         
         # Get challenges by group id number
-        [Parameter(
-            ParameterSetName = 'By Id')]
-        [int]$GroupId,
+        [Parameter(ParameterSetName = 'Ideation by Id with Authentication Header')]
+        [Parameter(ParameterSetName = 'Ideation by Id with Connection Profile')]
+        [Parameter(ParameterSetName = 'Ideation by Id with Connection File')]
+        [guid[]]$IdeationId,
         
         # Should I recurse into child groups?  Default is false
-        [Parameter(
-            Mandatory = $false
-        )]
         [switch]$Recurse = $false,
 
         # Community Domain to use (include trailing slash) Example: [https://yourdomain.telligenthosted.net/]
-        [Parameter(
-            Mandatory = $true, 
-            ParameterSetName = 'Ideas with Authentication Header'
-        )]
-        [Parameter(
-            Mandatory = $true, 
-            ParameterSetName = 'Ideas by Author ID with Authentication Header'
-        )]
+        [Parameter(ParameterSetName = 'All ideations with Authentication Header')]
+        [Parameter(ParameterSetName = 'Ideation by Name with Authentication Header')]
+        [Parameter(ParameterSetName = 'Ideation by Id with Authentication Header')]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
         [ValidatePattern('^(http:\/\/|https:\/\/)(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])\/$')]
@@ -68,30 +63,33 @@ function Get-VtChallenge {
         [string]$VtCommunity,
         
         # Authentication Header for the community
-        [Parameter(Mandatory = $true, ParameterSetName = 'Challenges with Authentication Header')]
-        [Parameter(Mandatory = $true, ParameterSetName = 'Ideas by Author ID with Authentication Header')]
+        [Parameter(ParameterSetName = 'All ideations with Authentication Header')]
+        [Parameter(ParameterSetName = 'Ideation by Name with Authentication Header')]
+        [Parameter(ParameterSetName = 'Ideation by Id with Authentication Header')]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
         [System.Collections.Hashtable]$VtAuthHeader,
     
-        [Parameter(Mandatory = $true, ParameterSetName = 'Ideas with Connection Profile')]
-        [Parameter(Mandatory = $true, ParameterSetName = 'Ideas by Author ID with Connection Profile')]
+        [Parameter(ParameterSetName = 'All ideations with Connection Profile')]
+        [Parameter(ParameterSetName = 'Ideation by Name with Connection Profile')]
+        [Parameter(ParameterSetName = 'Ideation by Id with Connection Profile')]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
         [System.Management.Automation.PSObject]$Connection,
     
         # File holding credentials.  By default is stores in your user profile \.vtPowerShell\DefaultCommunity.json
-        [Parameter(ParameterSetName = 'Ideas with Connection File')]
-        [Parameter(ParameterSetName = 'Ideas by Author ID with Connection File')]
+        [Parameter(ParameterSetName = 'All ideations with Connection File')]
+        [Parameter(ParameterSetName = 'Ideation by Name with Connection File')]
+        [Parameter(ParameterSetName = 'Ideation by Id with Connection File')]
         [string]$ProfilePath = ( $env:USERPROFILE ? ( Join-Path -Path $env:USERPROFILE -ChildPath ".vtPowerShell\DefaultCommunity.json" ) : ( Join-Path -Path $env:HOME -ChildPath ".vtPowerShell/DefaultCommunity.json" ) ),
 
         # Suppress the progress bar
         [Parameter()]
-        [switch]$SuppressProgressBar
-    
+        [switch]$SuppressProgressBar,
 
-    
-        
+        [Parameter()]
+        [Alias("Details")]
+        [switch]$ReturnDetails
     )
         
     BEGIN {
@@ -117,82 +115,116 @@ function Get-VtChallenge {
             }
         }
 
-                # Set default page index, page size, and add any other filters
-                $UriParameters = @{}
-                $UriParameters["PageIndex"] = 0
-                $UriParameters["PageSize"] = $BatchSize
+       # Set default page index, page size, and add any other filters
+       $UriParameters = @{}
+       $UriParameters["PageIndex"] = 0
+       $UriParameters["PageSize"] = $BatchSize
+
+       $PropertiesToReturn = @(
+           @{ Name = "IdeationId"; Expression = { $_.Id } }
+           'Name'
+           @{ Name = "Description"; Expression = { [System.Web.HttpUtility]::HtmlDecode( $_.Description ) } }
+           @{ Name = 'Url'; Expression = { "$( $_.Group.Url )/i/$( $_.ApplicationKey )"}}
+           @{ Name = "Enabled"; Expression = { $_.IsEnabled } }
+           'TotalPosts'
+           'RequiresCategory'
+           'AllowMultipleVotes'
+           @{ Name = "GroupName"; Expression = { [System.Web.HttpUtility]::HtmlDecode( $_.Group.Name ) } }
+           @{ Name = "GroupId"; Expression = { $_.Group.Id } }
+           'LastPostDate'
+       )
     }
         
     PROCESS {
 
 
-
-
-        switch ($PSCmdlet.ParameterSetName) {
-            "By Name" {
-                Write-Verbose -Message "Querying for Challenge by Name"
-                $UriSegment = "api.ashx/v2/ideas/challenges.json?Name=$( [System.Web.HTTPUtility]::UrlEncode( [System.Web.HTTPUtility]::HtmlEncode( $Name ) ) )"
-                $Uri = $VtCommunity + $UriSegment
-                if ( $ExactMatch -and $Recurse ) {
-                    $ChallengeId = Get-VtAll -Uri $Uri -AuthHeader $VtAuthHeader | Where-Object { $_.Name -eq [System.Web.HTTPUtility]::HtmlEncode( $Name ) } | Select-Object -ExpandProperty Id
-                    if ( $ChallengeId ) {
-                        Get-VtGroup -Community $VtCommunity -AuthHeader $VtAuthHeader -Id $GroupId -Recurse
-                    }
-                }
-                elseif ( $ExactMatch -and -not $Recurse ) {
-                    Get-VtAll -Uri $Uri -AuthHeader $VtAuthHeader | Where-Object { $_.Name -eq [System.Web.HTTPUtility]::HtmlEncode( $Name ) }
-                }
-                elseif ( $Recurse -and -not $ExactMatch ) {
-                    #Request-Groups -Uri $Uri -AuthHeader $VtAuthHeader
-                    $ChallengeIds = Get-VtAll -Uri $Uri -AuthHeader $VtAuthHeader | Select-Object -ExpandProperty Id
-                    ForEach ( $ChallengeId in $ChallengeIds ) {
-                        Get-VtChallenge -Community $VtCommunity -AuthHeader $VtAuthHeader -GroupId $GroupId -Recurse
-                    }
-                }
-                else {
-                    Get-VtAll -Uri $Uri -AuthHeader $VtAuthHeader
-                }
-                <#
-                    if ( $Recurse ) {
-                        $UriSegment += "&IncludeAllSubGroups=true"
-                    }
-                    $Uri = $VtCommunity + $UriSegment
-                    if ( -not $ExactMatch ) {
-                        Request-Groups -Uri $Uri -AuthHeader $VtAuthHeader
-                    } else {
-                        Request-Groups -Uri $Uri -AuthHeader $VtAuthHeader | Where-Object { $_.Name -eq [System.Web.HTTPUtility]::HtmlEncode( $Name ) } | Select-Object -ExpandProperty Id
-                    }
-                    #>
+        switch -Wildcard ( $PSCmdlet.ParameterSetName ) {
+            'Ideation by Name *' {
+                ForEach ( $Name in $GroupName ) {
+                    $Uri = 'api.ashx/v2/ideas/challenge.json'
+                    $UriParameters['GroupNameFilter'] = $Name
+                    $IdeationCount = 0
     
-            }
-            "By Id" {
-                Write-Verbose -Message "Querying for Challenge by Group ID"
-                $UriSegment = "api.ashx/v2/ideas/challenges.json?GroupId=$GroupId"
-                $Uri = $VtCommunity + $UriSegment
-                Get-VtAll -Uri $Uri -AuthHeader $VtAuthHeader
+                    do {
+                        Write-Verbose -Message "Making call with '$Uri'"
+                        # Get the list of groups with matching name from the call
+                        $IdeationsResponse = Invoke-RestMethod -Uri ( $Community + $Uri + '?' + ( $UriParameters | ConvertTo-QueryString ) ) -Headers $AuthHeaders
     
-                    
-                if ( $Recurse ) {
-                    Write-Verbose -Message "`tQuerying for children of Group by ID"
-                    $UriSegment = "api.ashx/v2/ideas/challenges.json?GroupId=$GroupId&IncludeAllSubGroups=true"
-                    $Uri = $VtCommunity + $UriSegment
-                    Get-VtAll -Uri $Uri -AuthHeader $VtAuthHeader
+                        if ( $IdeationsResponse ) {
+                            $IdeationCount += $IdeationsResponse.Challenges.Count
+                            # If we need an exact response on the name, then filter for only that exact group
+                            if ( $ExactMatch ) {
+                                $IdeationsResponse.Challenges = $IdeationsResponse.Challenges | Where-Object { [System.Web.HttpUtility]::HtmlDecode( $_.Name ) -eq $Name }
+                            }
+                            # Should we return everything?
+                            if ( $ReturnDetails ) {
+                                $IdeationsResponse.Challenges
+                            }
+                            else {
+                                $IdeationsResponse.Challenges | Select-Object -Property $PropertiesToReturn
+                            }
+                        }
+                        
+                        $UriParameters['PageIndex']++
+                        Write-Verbose -Message "Incrementing Page Index :: $( $UriParameters['PageIndex'] )"
+                    } while ( $IdeationCount -lt $IdeationsResponse.TotalCount )
+                        
                 }
             }
-            Default {
-                Write-Verbose -Message "Querying for all challenges"
-                $UriSegment = 'api.ashx/v2/ideas/challenges.json?IncludeAllSubGroups=true'
-                $UriSegment = 'api.ashx/v2/ideas/challenges.json'
-                $Uri = $VtCommunity + $UriSegment
-                Get-VtAll -Uri $Uri -AuthHeader $VtAuthHeader
+            'Ideation by Id *' {
+                ForEach ( $Id in $IdeationId ) {
+                        $Uri = "api.ashx/v2/ideas/challenge.json"
+                        $UriParameters['Id'] = $Id
+                    }
+    
+                    # Because everything is encoded in the URI, we don't need to send any $UriParameters
+                    Write-Verbose -Message "Making call with '$Uri'"
+                    $IdeationsResponse = Invoke-RestMethod -Uri ( $Community + $Uri ) -Headers $AuthHeaders -Verbose:$false
+                        
+                    if ( $IdeationsResponse.Challenge ) {
+                        if ( $ReturnDetails ) {
+                            $IdeationsResponse.Challenge
+                        }
+                        else {
+                            $IdeationsResponse.Challenge | Select-Object -Property $PropertiesToReturn
+                        }
+                    }
+                    else {
+                        Write-Warning -Message "No matching groups found for ID: $Id"
+                    }
+                }
+            default {
+                # No ForEach loop needed here because we are pulling all groups
+                $Uri = 'api.ashx/v2/ideas/challenges.json'
+                $IdeationCount = 0
+                do {
+                    $IdeationsResponse = Invoke-RestMethod -Uri ( $Community + $Uri + '?' + ( $UriParameters | ConvertTo-QueryString ) ) -Headers $AuthHeaders -Verbose:$false
+    
+                    if ( $ResolveParentName ) {
+                        # This calls itself to get the parent group name
+                        $IdeationsResponse.Challenges | Add-Member -MemberType ScriptProperty -Name "ParentGroupName" -Value { Get-VtGroup -GroupId $this.ParentGroupId | Select-Object -Property @{ Name = "Name"; Expression = { [System.Web.HttpUtility]::HtmlDecode( $_.Name ) } } | Select-Object -ExpandProperty Name } -Force
+                    }
+    
+                    if ( $IdeationsResponse ) {
+                        $IdeationCount += $IdeationsResponse.Challenges.Count
+                        if ( $ParentGroupId ) {
+                            $IdeationsResponse.Challenges = $IdeationsResponse.Challenges | Where-Object { $_.ParentGroupId -eq $ParentGroupId }
+                        }
+                        if ( $ReturnDetails ) {
+                            $IdeationsResponse.Challenges
+                        }
+                        else {
+                            $IdeationsResponse.Challenges | Select-Object -Property $PropertiesToReturn
+                        }
+                    }
+                    $UriParameters['PageIndex']++
+                    Write-Verbose -Message "Incrementing Page Index :: $( $UriParameters['PageIndex'] )"
+                } while ( $IdeationCount -lt $IdeationsResponse.TotalCount )
             }
         }
-        if ($PSCmdlet.ShouldProcess("On this target --> Target", "Did this thing --> Operation")) {
-                
-    
-        }
-    
+
     }
+    
         
     END {
             
